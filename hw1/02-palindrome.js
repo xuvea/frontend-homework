99ed6241328ca1f4f7d1848b9feee9061540a068
xuvea <90873349+xuvea@users.noreply.github.com>
2023-04-11 20:53:33 -0700

    Completed exercise 02, implementing the palindome checker. Did styling changes to the HTML using bootstrap.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,27 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+function handleInput() {
+  if (parseInt(elem.value) >= 0) {
+    var original = elem.value;
+    var reversed = original.split("");
+    reversed = reversed.reverse();
+    reversed = reversed.join("");
+    element = document.getElementById("response");
+
+    if (original.valueOf() === reversed.valueOf()) {
+      element.classList.remove("text-danger");
+      element.classList.add("text-success");
+      element.innerHTML = "Yes. This is a palidrome!";
+    } else {
+      element.classList.remove("text-success");
+      element.classList.add("text-danger");
+      element.innerHTML = "No. Try Again.";
+    }
+  } else {
+    element.classList.remove("text-danger");
+    element.classList.remove("text-success");
+    element.innerHTML = "Not a positive number, try again.";
+  }
+}
