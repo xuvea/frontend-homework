a93d669a13787bdba48863dfb6feb34e028bc14e
xuvea <90873349+xuvea@users.noreply.github.com>
2023-04-14 14:27:51 -0700

    Added a check for non number inputs.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -3,12 +3,23 @@ const elem = document.querySelector('input');
 elem.addEventListener('input', handleInput);
 
 function handleInput() {
+  element = document.getElementById('response');
+  console.log(elem.value);
+  if (isNaN(elem.value)) {
+    element.classList.remove('text-success');
+    element.classList.add('text-danger');
+    element.innerHTML = 'This is not a number, please try again.';
+    return;
+  }
+  if (elem.value == '') {
+    element.innerHTML = '';
+    return;
+  }
   if (parseInt(elem.value) >= 0) {
     var original = elem.value;
     var reversed = original.split('');
     reversed = reversed.reverse();
     reversed = reversed.join('');
-    element = document.getElementById('response');
 
     if (original.valueOf() === reversed.valueOf()) {
       element.classList.remove('text-danger');
@@ -20,8 +31,8 @@ function handleInput() {
       element.innerHTML = 'No. Try Again.';
     }
   } else {
-    element.classList.remove('text-danger');
     element.classList.remove('text-success');
+    element.classList.add('text-danger');
     element.innerHTML = 'Not a positive number, try again.';
   }
 }
