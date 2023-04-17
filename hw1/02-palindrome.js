84ff3b876f749f02bfb1c0b0d9d20a6d51872603
Alissa Friel <90873349+xuvea@users.noreply.github.com>
2023-04-17 12:32:15 -0700

    Homework1 (#1)
    * Completed the CSS Diner game and filled out 01-selectors file with my answers.
    
    * Completed exercise 02, implementing the palindome checker. Did styling changes to the HTML using bootstrap.
    
    * Updated JavaScript sytling of strings from double quotes to single quotes.
    
    * Added a check for non number inputs.
    
    * Removed unnesessary lines of code.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,37 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+
+function handleInput() {
+  element = document.getElementById('response');
+  if (isNaN(elem.value)) {
+    element.classList.remove('text-success');
+    element.classList.add('text-danger');
+    element.innerHTML = 'This is not a number, please try again.';
+    return;
+  }
+  if (elem.value == '') {
+    element.innerHTML = '';
+    return;
+  }
+  if (parseInt(elem.value) >= 0) {
+    var original = elem.value;
+    var reversed = original.split('');
+    reversed = reversed.reverse();
+    reversed = reversed.join('');
+
+    if (original.valueOf() === reversed.valueOf()) {
+      element.classList.remove('text-danger');
+      element.classList.add('text-success');
+      element.innerHTML = 'Yes. This is a palidrome!';
+    } else {
+      element.classList.remove('text-success');
+      element.classList.add('text-danger');
+      element.innerHTML = 'No. Try Again.';
+    }
+  } else {
+    element.classList.remove('text-success');
+    element.classList.add('text-danger');
+    element.innerHTML = 'Not a positive number, try again.';
+  }
+}
