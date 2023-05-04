e922ef78d58055d323fcb37b2161b585f841a5f5
Alissa Friel <90873349+xuvea@users.noreply.github.com>
2023-05-04 15:39:06 -0700

    Homework2 (#2)
    * Added answers that I got for the Grid Garden game.
    
    * Added answers that I got for the Flexbox Froggy game.
    
    * Completed the count exercise functionality and styling.
    
    * Completed background exercise functionality and styling.
    
    * Completed form exercise functionality and styling.
    
    * Removed unnecessary console.log and made minor changes to formating.

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,47 @@
 // Add your code here
+let interval;
+const time = document.querySelector('input');
+
+window.onload = (event) => {
+randomColor();  
+};
+
+function changeBackground() {
+  if (!interval) {
+    if (time.value >= 1) {
+      interval = setInterval(randomColor, time.value * 1000);
+    }
+    else {
+      interval = setInterval(randomColor, 0.5 * 1000);
+    }
+  }
+}
+
+function randomColor() {
+  const randomBackground = document.getElementById('page-background')
+  const range = 266;
+  randomBackground.style.backgroundColor = `rgba(${Math.random() * range}, 
+  ${Math.random() * range}, 
+  ${Math.random() * range}, 
+  ${Math.random() * 0.8})`;
+}
+
+function startAndStop() {
+  mode = document.getElementById('start-and-stop');
+  if(mode.innerHTML === 'Start') {
+    mode.innerHTML = 'Stop';
+    mode.classList.remove('btn-primary');
+    mode.classList.add('btn-danger');
+    
+    changeBackground();
+  }
+  else if(mode.innerHTML === 'Stop') {
+    mode.innerHTML = 'Start';
+    mode.classList.remove('btn-danger');
+    mode.classList.add('btn-primary');
+    clearInterval(interval);
+    interval = null;
+  }
+} 
+
+document.getElementById('start-and-stop').addEventListener('click', startAndStop);
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,24 @@
 // Add your code here
+function printToConsole(event) {
+  event.preventDefault();
+
+  const name = document.getElementById('fullName').value;
+  const email = document.getElementById('email').value;
+  const registration = document.getElementById('registration').value;
+  const course1 = document.getElementById('course1').checked;
+  const course2 = document.getElementById('course2').checked;
+  const course3 = document.getElementById('course3').checked;
+  const feedback = document.getElementById('feedback').value;
+  console.log('**** Form Submission ****');
+  console.log('Full Name: ' + name);
+  console.log('Email: ' + email);
+  console.log('Registration Status: ' + (registration === 'Choose an Option' ? 'None' : registration));
+  console.log('Classes Taken:')
+  if (course1) console.log('  Programming Languages');
+  if (course2) console.log('  Operating Systems');
+  if (course3) console.log('  Full Stack Web Development');
+  console.log('Form Feedback:')
+  console.log(' ' + (feedback === '' ? 'N/A' : feedback));
+}
+
+document.querySelector('form').addEventListener('submit', printToConsole);
